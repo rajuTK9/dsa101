b9275d20338e43e32df6187d419d604d891e08aa
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-02-14 14:46:51 +0530

    14. Mongoose

--- server/app.js
+++ server/app.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
+const DB =
+  "mongodb+srv://rajutk:tmkBDFA@dsa101.4cd4s4c.mongodb.net/?retryWrites=true&w=majority";
 const PORT = 4000;
+
+mongoose.set("strictQuery", true);
+
+mongoose
+  .connect(DB)
+  .then(() => {
+    console.log("Connection successfull.");
+  })
+  .catch((err) => {
+    console.log("No connection \n", err);
+  });
 
 app.get("/", (req, res) => {
   res.send("DSA101 Home");
