11c72a2e40dbb31e8d552ce07a3987ca6541feda
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-02-16 20:36:39 +0530

    Navbar and other components created successfully.

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,14 +1,42 @@
 import React from "react";
-import { GoogleLogin } from "@react-oauth/google";
-import jwt_decode from "jwt-decode";
-import { GoogleOAuthProvider } from "@react-oauth/google";
+// import { GoogleLogin } from "@react-oauth/google";
+// import jwt_decode from "jwt-decode";
+// import { GoogleOAuthProvider } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
 import "./Login.css";
+import axios from "axios";
 
 export default function Login() {
+  const login = useGoogleLogin({
+    onSuccess: async (respose) => {
+      try {
+        const res = await axios.get(
+          "https://www.googleapis.com/oauth2/v3/userinfo",
+          {
+            headers: {
+              Authorization: `Bearer ${respose.access_token}`,
+            },
+          }
+        );
+
+        console.log(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    },
+  });
   return (
     <div>
-      <GoogleOAuthProvider clientId="484839371598-8359bacl69e86c2oa2hotfbqmug26s74.apps.googleusercontent.com">
-        <GoogleLogin
+      {/* <GoogleOAuthProvider clientId="484839371598-8359bacl69e86c2oa2hotfbqmug26s74.apps.googleusercontent.com"> */}
+      <button className="btn btn-primary google-login-btn" onClick={login}>
+        <img
+          src="./assets/icons/google-logo.svg"
+          alt="G"
+          className="google-logo"
+        />
+        Login
+      </button>
+      {/* <GoogleLogin
           onSuccess={(credentialResponse) => {
             console.log(credentialResponse);
             var token = credentialResponse.credential;
@@ -18,8 +46,8 @@ export default function Login() {
           onError={() => {
             console.log("Login Failed");
           }}
-        />
-      </GoogleOAuthProvider>
+        /> */}
+      {/* </GoogleOAuthProvider> */}
     </div>
   );
 }
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,12 +1,17 @@
 import React from "react";
 import Login from "../Login/Login";
 import "./Navbar.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 export default function Navbar() {
   return (
-    <div>
-      Navbar
-      <Login />
+    <div className="navbar">
+      <div className="logo">
+        <img src="assets/logo-full.svg" alt="DSA101" className="logo-img" />
+      </div>
+      <GoogleOAuthProvider clientId="484839371598-8359bacl69e86c2oa2hotfbqmug26s74.apps.googleusercontent.com">
+        <Login />
+      </GoogleOAuthProvider>
     </div>
   );
 }
