c163717be42351a267a9ef026908ce4c47da7bf0
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-03-21 04:25:41 +0530

    Toastify used instead of alert in payment page

--- client/src/components/PayMe/PayMe.js
+++ client/src/components/PayMe/PayMe.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "./PayMe.css";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function PayMe() {
   const [amount, setamount] = useState("");
@@ -7,7 +9,7 @@ export default function PayMe() {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (amount === "") {
-      alert("please enter amount");
+      toast.warn("please enter amount");
     } else {
       var options = {
         key: process.env.REACT_APP_RAZORPAY_KEY,
@@ -17,12 +19,7 @@ export default function PayMe() {
         name: "DSA101",
         description: "Buy Me A Coffee ☕️",
         handler: function (response) {
-          alert(response.razorpay_payment_id);
-        },
-        prefill: {
-          name: "Raju Tukadiya",
-          email: "tukadiyar9@gmail.com",
-          contact: "8460639501",
+          toast.success("Thanks for the Coffee ☺️");
         },
         notes: {
           address: "BH-6, LPU, Phagwara, Punjab - 144411",
@@ -36,33 +33,37 @@ export default function PayMe() {
     }
   };
   return (
-    <div className="payme-container">
-      <h1 className="heading">
-        If you found my project helpful, please consider buying me a coffee! ☕️
-      </h1>
-      <form className="payment-form" onSubmit={handleSubmit}>
-        <div className="form-field">
-          <label htmlFor="amount">
-            Amount (
-            <span style={{ color: "#7282fe", fontWeight: "bold" }}>$</span>) :
-          </label>
-          <input
-            type="number"
-            id="amount"
-            name="amount"
-            value={amount}
-            onChange={(e) => setamount(e.target.value)}
-            min="1"
-            step="1"
-          />
-        </div>
-        <button className="btn-primary payment-btn">
-          Proceed{" "}
-          <div>
-            <img src="/assets/icons/arrow-right-white.svg" alt="" />
+    <>
+      <div className="payme-container">
+        <h1 className="heading">
+          If you found my project helpful, please consider buying me a coffee!
+          ☕️
+        </h1>
+        <form className="payment-form" onSubmit={handleSubmit}>
+          <div className="form-field">
+            <label htmlFor="amount">
+              Amount (
+              <span style={{ color: "#7282fe", fontWeight: "bold" }}>$</span>) :
+            </label>
+            <input
+              type="number"
+              id="amount"
+              name="amount"
+              value={amount}
+              onChange={(e) => setamount(e.target.value)}
+              min="1"
+              step="1"
+            />
           </div>
-        </button>
-      </form>
-    </div>
+          <button className="btn-primary payment-btn">
+            Proceed{" "}
+            <div>
+              <img src="/assets/icons/arrow-right-white.svg" alt="" />
+            </div>
+          </button>
+        </form>
+      </div>
+      <ToastContainer />
+    </>
   );
 }
