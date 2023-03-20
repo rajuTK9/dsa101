40127398f7782eeb9668bf127693b1344c543b8c
TukadiyaRaju <raju.tukadiya@gmail.com>
2023-03-21 04:12:45 +0530

    Razorpay Payment Gateway Setup

---
+++ client/src/components/PayMe/PayMe.js
@@ -0,0 +1,68 @@
+import React, { useState } from "react";
+import "./PayMe.css";
+
+export default function PayMe() {
+  const [amount, setamount] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (amount === "") {
+      alert("please enter amount");
+    } else {
+      var options = {
+        key: process.env.REACT_APP_RAZORPAY_KEY,
+        key_secret: process.env.REACT_APP_RAZORPAY_SECRET,
+        amount: amount * 100,
+        currency: "USD",
+        name: "DSA101",
+        description: "Buy Me A Coffee ☕️",
+        handler: function (response) {
+          alert(response.razorpay_payment_id);
+        },
+        prefill: {
+          name: "Raju Tukadiya",
+          email: "tukadiyar9@gmail.com",
+          contact: "8460639501",
+        },
+        notes: {
+          address: "BH-6, LPU, Phagwara, Punjab - 144411",
+        },
+        theme: {
+          color: "#7282fe",
+        },
+      };
+      var pay = new window.Razorpay(options);
+      pay.open();
+    }
+  };
+  return (
+    <div className="payme-container">
+      <h1 className="heading">
+        If you found my project helpful, please consider buying me a coffee! ☕️
+      </h1>
+      <form className="payment-form" onSubmit={handleSubmit}>
+        <div className="form-field">
+          <label htmlFor="amount">
+            Amount (
+            <span style={{ color: "#7282fe", fontWeight: "bold" }}>$</span>) :
+          </label>
+          <input
+            type="number"
+            id="amount"
+            name="amount"
+            value={amount}
+            onChange={(e) => setamount(e.target.value)}
+            min="1"
+            step="1"
+          />
+        </div>
+        <button className="btn-primary payment-btn">
+          Proceed{" "}
+          <div>
+            <img src="/assets/icons/arrow-right-white.svg" alt="" />
+          </div>
+        </button>
+      </form>
+    </div>
+  );
+}
